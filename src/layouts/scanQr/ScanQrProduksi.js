757c52cf127efb090ff8c7eb9648ec61a8ed81f6
Kezia Natalia <kezia.natalia@ui.ac.id>
2024-07-30 16:26:57 +0700

    scan uploaded image

--- src/layouts/scanQr/ScanQrProduksi.js
+++ src/layouts/scanQr/ScanQrProduksi.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
 import QrScanner from 'react-qr-scanner';
+import QrScannerLib from 'qr-scanner';
 import "./QrStyles.css";
 import QrFrame from "./qr-frame.svg";
 
@@ -21,21 +22,24 @@ function ScanQrProduksi() {
     const navigate = useNavigate();
     const [scanning, setScanning] = useState(false);
     const [result, setResult] = useState('No result');
+    const [legacyMode, setLegacyMode] = useState(false);
 
     const user = JSON.parse(localStorage.getItem("user"));
 
     const handleScan = useCallback((data) => {
       if (data) {
           console.log(data);
+          const id = data.text || data;
           setResult(data);
 
           if (user.role === "petugasProduksi"){
-            navigate(`/produksi/${data.text}/isi-data`)
+            navigate(`/produksi/${id}/isi-data`)
           } else {
-            navigate(`/produksi/${data.text}`)
+            navigate(`/produksi/${id}`)
           }
 
           setScanning(false);
+          setLegacyMode(false);
       }
   }, [navigate]);
 
@@ -50,77 +54,100 @@ function ScanQrProduksi() {
       position: 'relative'
     };
 
-    // useEffect(() => {
-    //     const html5QrCodeScanner = new Html5QrcodeScanner(
-    //       "reader",
-    //       { fps: 10, qrbox: 250 },
-    //       false
-    //     );
-    
-    //     const onScanSuccess = (decodedText) => {
-    //       // Redirect to the detail page with the scanned QR code data
-    //       navigate(`/produksi/${decodedText}/isi-data`);
-    //       html5QrCodeScanner.clear();
-    //     };
-    
-    //     const onScanFailure = (error) => {
-    //       console.warn(`QR code scan error: ${error}`);
-    //     };
-    
-    //     html5QrCodeScanner.render(onScanSuccess, onScanFailure);
-    
-    //     return () => {
-    //       html5QrCodeScanner.clear();
-    //     };
-    //   }, []);
+    const handleFileChange = async (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = async () => {
+          const img = new Image();
+          img.src = reader.result;
+          img.onload = async () => {
+            try {
+              const result = await QrScannerLib.scanImage(img, { returnDetailedScanResult: true });
+              if (result && result.data) {
+                handleScan(result.data);
+              } else {
+                handleError('No QR code found.');
+              }
+            } catch (err) {
+              handleError(err);
+            }
+          };
+        };
+        reader.readAsDataURL(file);
+      }
+    };
 
     return(
-        <DashboardLayout>
-            <DashboardNavbar />
-            <MDBox mt={3} mb={3}>
-              <Grid container spacing={3} justifyContent="center">
-                <Grid item xs={12} lg={8}>
-                  <Card>
-                    <MDBox p={3} align="center">
-                        <MDTypography variant="h4">Scan QR Panen</MDTypography>
-                    </MDBox>
-                      {!scanning ? (
-                        <MDBox align="center" mt={5} mb={2}>
-                          <MDButton variant="gradient" color="primary" onClick={() => setScanning(true)}>Start</MDButton>
-                        </MDBox>
-                      ) : (
-                        <MDBox pt={2} px={5}>
-                          <Grid container spacing={3} align='center'>
-                            <Grid item xs={12} md={12}>
-                              <QrScanner
-                                delay={300}
-                                style={previewStyle}
-                                onError={handleError}
-                                onScan={handleScan}
-                              />
-                              <img 
-                                src={QrFrame} 
-                                alt="Qr Frame" 
-                                className="scan-box-overlay"
-                              />
-                            </Grid>
-                            <Grid item xs={12} md={12} mb={2}>
-                              <MDButton variant="gradient" color="error" onClick={() => setScanning(false)}>Stop</MDButton>
-                            </Grid>
-                          
-                          </Grid>
-                        </MDBox>
-                      )}   
-                  </Card>
-                </Grid> 
-              </Grid>
-            
-            
-            </MDBox>
-            
-
-            
-        </DashboardLayout>
+      <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox mt={3} mb={3}>
+        <Grid container spacing={3} justifyContent="center">
+          <Grid item xs={12} lg={8}>
+            <Card>
+              <MDBox p={3} align="center">
+                  <MDTypography variant="h4">Scan QR Panen</MDTypography>
+              </MDBox>
+                {!scanning ? (
+                  <MDBox align="center" mt={5} mb={2}>
+                    <Grid container spacing={3} align='center'>
+                      <Grid item xs={12} md={12}>
+                        <MDButton variant="gradient" color="primary" onClick={() => setScanning(true)}>Start</MDButton>
+                      </Grid>
+                      <Grid item xs={12} md={12}>
+                        <MDButton 
+                          component="span" 
+                          onClick={() => document.getElementById('upload').click()}
+                          color="info"
+                          // sx={{ 
+                          //   // color: 'blue', 
+                          //   textDecoration: 'underline',
+                          //   // background: 'none', 
+                          //   // border: 'none', 
+                          //   // padding: 0, 
+                          //   cursor: 'pointer',
+                          //   // minWidth: 0 
+                          // }}
+                          >Scan an Image File
+                        </MDButton>
+                          <input
+                            type="file"
+                            id="upload"
+                            accept="image/*"
+                            onChange={handleFileChange}
+                            style={{ display: 'none' }}
+                          />
+                      </Grid>
+                    </Grid>    
+                  </MDBox>
+                ) : (
+                  <MDBox pt={2} px={5}>
+                    <Grid container spacing={3} align='center'>
+                      <Grid item xs={12} md={12}>
+                        <QrScanner
+                          delay={300}
+                          style={previewStyle}
+                          onError={handleError}
+                          onScan={handleScan}
+                          facingMode="environment"
+                        />
+                        <img 
+                          src={QrFrame} 
+                          alt="Qr Frame" 
+                          className="scan-box-overlay"
+                        />
+                      </Grid>
+                      <Grid item xs={12} md={12} mb={2}>
+                        <MDButton variant="gradient" color="error" onClick={() => setScanning(false)}>Stop</MDButton>
+                      </Grid>
+                    </Grid>
+                  </MDBox>
+                )}   
+            </Card>
+          </Grid> 
+        </Grid> 
+      </MDBox>    
+    </DashboardLayout>
     )
 }
 
